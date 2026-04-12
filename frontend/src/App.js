a62613ae4824f63761bbda63c1049c9c1f1a5ee6
Girjendra <shivaji12889@gmail.com>
2026-04-12 09:18:28 +0530

    add frontend UI with register and login pages

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+function Home() {
+  return <h1 style={{ textAlign: "center", marginTop: "50px" }}>
+    Welcome to FOSSEE Workshop Portal
+  </h1>;
+}
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
